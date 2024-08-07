9c9848878d6c75b288ed4ca075768a7ee3186adb
Brian Salchert <brisalchert@gmail.com>
2024-08-07 12:26:51 -0400

    Performed and plotted 10,000 random walks

--- intermediate_python/hacker_statistics.py
+++ intermediate_python/hacker_statistics.py
@@ -4,29 +4,42 @@ import matplotlib.pyplot as plt
 # Set seed for reproducibility
 np.random.seed(123)
 
-# Initialize random_walk
-random_walk = [0]
+# Initialize list of final step values
+all_walks = []
 
-# Complete the ___
-for x in range(100):
-    # Set step: last element in random_walk
-    step = random_walk[-1]
+# Perform 10,000 random walks
+for i in range(10000):
+    # Initialize random_walk
+    random_walk = [0]
 
-    # Roll the dice
-    dice = np.random.randint(1, 7)
+    # Perform random walk
+    for x in range(100):
+        # Set step: last element in random_walk
+        step = random_walk[-1]
 
-    # Determine next step
-    if dice <= 2:
-        # Do not allow negative steps
-        step = max(step - 1, 0)
-    elif dice <= 5:
-        step = step + 1
-    else:
-        step = step + np.random.randint(1, 7)
+        # Roll the dice
+        dice = np.random.randint(1, 7)
 
-    # append next_step to random_walk
-    random_walk.append(step)
+        # Determine next step
+        if dice <= 2:
+            # Do not allow negative steps
+            step = max(step - 1, 0)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1, 7)
 
-# Plot the random walk
-plt.plot(random_walk)
+        # append next_step to random_walk
+        random_walk.append(step)
+
+    # Add the walk to all_walks
+    all_walks.append(random_walk)
+
+# Convert final_steps to numpy array
+np_all_walks = np.array(all_walks)
+
+# Transpose np_final_steps
+np_all_walks_transposed = np.transpose(np_all_walks)
+
+plt.plot(np_all_walks_transposed)
 plt.show()
