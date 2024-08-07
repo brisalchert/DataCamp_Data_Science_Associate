7407f24160bcec65829422738c7c994b5c975b97
Brian Salchert <brisalchert@gmail.com>
2024-08-07 12:42:31 -0400

    Added header comment

--- intermediate_python/hacker_statistics.py
+++ intermediate_python/hacker_statistics.py
@@ -1,3 +1,16 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# hacker_statistics.py
+#
+# Python script for simulating a game about climbing the Empire State Building. The player climbs the building by
+# rolling a die. On a 1 or 2, the player descends one step (unless they are at the bottom). On a 3, 4, or 5, the player
+# ascends one step. On a 6, the player rolls a die and ascends the number of steps on the die. For each roll (excluding
+# the additional rolls for rolling a 6), there is a 0.1% chance that the player will fall to the bottom.
+#
+# The player rolls 100 times per game, and the random walk of their step numbers is recorded. These random walks are
+# performed 10,000 times, and the results are used to plot a histogram of the endpoints of each walk. Then, the
+# probability of reaching step 60 on any given walk is calculated and printed out.
+# ----------------------------------------------------------------------------------------------------------------------
+
 import numpy as np
 import matplotlib.pyplot as plt
 
