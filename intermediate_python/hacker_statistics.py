013495dec51ea17571da762b14f5d211f3f2a8d7
Brian Salchert <brisalchert@gmail.com>
2024-08-07 12:11:34 -0400

    Fixed formatting issues

--- intermediate_python/hacker_statistics.py
+++ intermediate_python/hacker_statistics.py
@@ -8,12 +8,12 @@ np.random.seed(123)
 random_walk = [0]
 
 # Complete the ___
-for x in range(100) :
+for x in range(100):
     # Set step: last element in random_walk
     step = random_walk[-1]
 
     # Roll the dice
-    dice = np.random.randint(1,7)
+    dice = np.random.randint(1, 7)
 
     # Determine next step
     if dice <= 2:
@@ -22,7 +22,7 @@ for x in range(100) :
     elif dice <= 5:
         step = step + 1
     else:
-        step = step + np.random.randint(1,7)
+        step = step + np.random.randint(1, 7)
 
     # append next_step to random_walk
     random_walk.append(step)
