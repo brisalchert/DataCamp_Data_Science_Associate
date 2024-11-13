fb986f73d1e14853622bc6d107c7be8650994dfd
Brian Salchert <brisalchert@gmail.com>
2024-11-13 13:22:27 -0500

    Update header comment

--- hypothesis_testing_soccer/hypothesis_testing_soccer.py
+++ hypothesis_testing_soccer/hypothesis_testing_soccer.py
@@ -12,6 +12,15 @@
 #
 #  where p_val is the p-value and result is either the string "fail to reject" or "reject", depending on the result
 #  of the test.
+#
+#  The question you are trying to determine the answer to is:
+#
+#    Are more goals scored in women's international soccer matches than men's?
+#
+#  You assume a 10% significance level, and use the following null and alternative hypotheses:
+#
+#    H0: The mean number of goals scored in women's international soccer matches is the same as men's.
+#    HA: The mean number of goals scored in women's international soccer matches is greater than men's.
 #-----------------------------------------------------------------------------------------------------------------------
 
 # Import required modules
