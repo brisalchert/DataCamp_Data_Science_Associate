4e852ccc9616f8795e6b28726b06c559be5a786f
Brian Salchert <brisalchert@gmail.com>
2024-11-16 17:07:15 -0500

    Add starter code

--- clustering_penguin_species/clustering_penguin_species.py
+++ clustering_penguin_species/clustering_penguin_species.py
@@ -25,4 +25,12 @@
 #    columns.
 #-----------------------------------------------------------------------------------------------------------------------
 
+# Import Required Packages
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
+# Loading and examining the dataset
+penguins_df = pd.read_csv("penguins.csv")
+print(penguins_df.head())
