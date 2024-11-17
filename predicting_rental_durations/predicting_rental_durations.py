6006897dd4ce2b9e258831f01109fa4fbb440194
Brian Salchert <brisalchert@gmail.com>
2024-11-17 14:55:34 -0500

    Split training and test data

--- predicting_rental_durations/predicting_rental_durations.py
+++ predicting_rental_durations/predicting_rental_durations.py
@@ -66,3 +66,6 @@ X = rentals[[
 
 # Create series of targets for regression model
 y = rentals["rental_length_days"]
+
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=9)
