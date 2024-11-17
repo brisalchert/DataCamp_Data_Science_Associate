c1d5984999204142a41836e2b74591bf8af710ec
Brian Salchert <brisalchert@gmail.com>
2024-11-17 14:26:53 -0500

    Create predicting_rental_durations.py

---
+++ predicting_rental_durations/predicting_rental_durations.py
@@ -0,0 +1,27 @@
+#-----------------------------------------------------------------------------------------------------------------------
+#  In this project, you will use regression models to predict the number of days a customer rents DVDs for.
+#
+#  As with most data science projects, you will need to pre-process the data provided, in this case, a csv file called
+#  rental_info.csv. Specifically, you need to:
+#
+#  - Read in the csv file rental_info.csv using pandas.
+#  - Create a column named "rental_length_days" using the columns "return_date" and "rental_date", and add it to the
+#    pandas DataFrame. This column should contain information on how many days a DVD has been rented by a customer.
+#  - Create two columns of dummy variables from "special_features", which takes the value of 1 when:
+#    - The value is "Deleted Scenes", storing as a column called "deleted_scenes".
+#    - The value is "Behind the Scenes", storing as a column called "behind_the_scenes".
+#  - Make a pandas DataFrame called X containing all the appropriate features you can use to run the regression models,
+#    avoiding columns that leak data about the target.
+#  - Choose the "rental_length_days" as the target column and save it as a pandas Series called y.
+#
+#  Following the preprocessing you will need to:
+#
+#  - Split the data into X_train, y_train, X_test, and y_test train and test sets, avoiding any features that leak data
+#    about the target variable, and include 20% of the total data in the test set.
+#-----------------------------------------------------------------------------------------------------------------------
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
